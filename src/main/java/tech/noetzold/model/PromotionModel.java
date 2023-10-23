dd2f622d64b41bdfa56dc264c2c6ee4671b46be2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-23 16:35:39 -0300

    Update PromotionModel.java

--- src/main/java/tech/noetzold/model/PromotionModel.java
+++ src/main/java/tech/noetzold/model/PromotionModel.java
@@ -1,4 +1,30 @@
 package tech.noetzold.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class PromotionModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID promoId;
+    private String promoName;
+    private String promoDescription;
+    private ValueType valueType;
+    private Double value;
+    private PromotionType promotionType;
+    private String promoCompDesc;
+    private Double promoCompType;
+    private Double promoDtlId;
+    private String rules;
+    private Date startDate;
+    private Date endDate;
+    private String applyToCode;
+    private Double discountLimit;
+    private String exceptionParentId;
+
 }
