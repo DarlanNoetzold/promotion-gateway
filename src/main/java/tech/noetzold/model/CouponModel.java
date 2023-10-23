9ba7d9f082dcdb8d2853b42ef5587596c272af61
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-23 16:35:35 -0300

    Update CouponModel.java

--- src/main/java/tech/noetzold/model/CouponModel.java
+++ src/main/java/tech/noetzold/model/CouponModel.java
@@ -1,4 +1,21 @@
 package tech.noetzold.model;
 
+import jakarta.persistence.*;
+import org.wildfly.common.annotation.NotNull;
+
+import java.util.UUID;
+
 public class CouponModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID couponId;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "promoId")
+    private PromotionModel promotion;
+
+    private String code;
+    
 }
