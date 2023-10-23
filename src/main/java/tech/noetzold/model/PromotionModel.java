7ba5fc5e1900d16a34a3883d47204e1bcf77da94
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-23 17:06:11 -0300

    Update PromotionModel.java

--- src/main/java/tech/noetzold/model/PromotionModel.java
+++ src/main/java/tech/noetzold/model/PromotionModel.java
@@ -1,12 +1,20 @@
 package tech.noetzold.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class PromotionModel {
 
     @Id
