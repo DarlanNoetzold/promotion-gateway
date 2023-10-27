3efdea1ec24b0159c98d3546c353b4eaebf3b1af
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-27 08:45:21 -0300

    Update PromotionControllerTest.java

--- src/test/java/tech/noetzold/PromotionControllerTest.java
+++ src/test/java/tech/noetzold/PromotionControllerTest.java
@@ -2,8 +2,12 @@ package tech.noetzold;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
+import io.quarkus.test.security.TestSecurity;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import tech.noetzold.controller.PromotionController;
 
 import java.util.HashMap;
@@ -22,7 +26,7 @@ public class PromotionControllerTest {
         final String username = "admin";
         final String password = "admin";
 
-        final String tokenEndpoint = "http://localhost:8180/realms/quarkus/protocol/openid-connect/token";
+        final String tokenEndpoint = "http://localhost:8180/realms/quarkus1/protocol/openid-connect/token";
 
         final Map<String, String> requestData = new HashMap<>();
         requestData.put("username", username);
@@ -39,5 +43,59 @@ public class PromotionControllerTest {
         response.then().statusCode(200);
 
         this.accessToken = response.jsonPath().getString("access_token");
+    }
+
+    @Test
+    @Order(1)
+    @TestSecurity(user = "admin", roles = {"admin"})
+    public void testSavePromotion() {
+        given()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .body("{ \"promoName\": \"Test Promotion\", \"valueType\": \"PERCENT\", \"value\": 20.0, \"promoDescription\": \"Test Description\", \"promotionType\": \"ITEM\", \"promoCompDesc\": \"Test Comp Desc\", \"promoCompType\": 0.5, \"promoDtlId\": 1.0, \"rules\": \"Test Rules\", \"startDate\": \"2023-10-26\", \"endDate\": \"2023-10-27\", \"applyToCode\": \"TESTCODE\", \"discountLimit\": 50.0, \"exceptionParentId\": \"TESTPARENT\" }")
+                .when()
+                .post("http://localhost:5000/api/promo/v1/promotion")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    @Order(2)
+    @TestSecurity(user = "admin", roles = {"admin"})
+    public void testGetPromotionModelById() {
+        given()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .when()
+                .get("http://localhost:5000/api/promo/v1/promotion/{id}", "8b71d0c5-9f98-4c4a-815c-55e4c0518f6a")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @Order(3)
+    @TestSecurity(user = "admin", roles = {"admin"})
+    public void testUpdatePromotion() {
+        given()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .body("{ \"promoName\": \"Updated Promotion\", \"promoDescription\": \"Updated Description\" }")
+                .when()
+                .put("http://localhost:5000/api/promo/v1/promotion/{id}", "8b71d0c5-9f98-4c4a-815c-55e4c0518f6a")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @Order(4)
+    @TestSecurity(user = "admin", roles = {"admin"})
+    public void testDeletePromotion() {
+        given()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("http://localhost:5000/api/promo/v1/promotion/{id}", "d3e6dbb6-4f9c-4b4d-843e-8f0d8bdc5e58")
+                .then()
+                .statusCode(200);
     }
 }
